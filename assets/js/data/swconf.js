a17aa34079b5dfac61683d580909bb4d1cb5c1e6
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2024-11-26 15:59:38 +0000

    disabled test site

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1732636756',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/project/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
