0b25c425d1d2795facba06544ad02054772d6ac5
RuffaloLavoisier <RuffaloLavoisier@gmail.com>
2026-04-13 00:10:59 +0000

    Ruffalo Blog 2026-04-13 00:10:40

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1775952572',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/project/', '/interest/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1776039038',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/project/', '/interest/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
